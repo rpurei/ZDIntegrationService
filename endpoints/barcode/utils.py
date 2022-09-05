ff937ea8ccc7b6a65593ea3eb36a8655e38e1929
Purey Roman <purey.rp@zdmail.ru>
2022-09-05 11:33:26 +0300

    Fix bytes representation on base64

--- endpoints/barcode/utils.py
+++ endpoints/barcode/utils.py
@@ -23,7 +23,7 @@ def doc_to_base64(file_name: str):
     encoded_string = ''
     try:
         with open(file_name, 'rb') as image_file:
-            encoded_string = base64.b64encode(image_file.read())
+            encoded_string = base64.b64encode(image_file.read()).decode('ascii')
     except Exception as err:
         logger.error(f'For file:"{file_name}" error "{str(err)}" occured!!!')
     finally:
