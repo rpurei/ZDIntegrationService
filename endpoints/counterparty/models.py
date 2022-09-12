9963a739ec222707ac337b741ac9076ae869b335
Purey Roman <purey.rp@zdmail.ru>
2022-09-12 17:14:29 +0300

    Updated ConterpartyCheck model and added GET route

--- endpoints/counterparty/models.py
+++ endpoints/counterparty/models.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Union
 
 
 class ConturAPIGet(BaseModel):
-    type: str
-    key: str
+    api_key: str
     inn: Union[str, None] = None
     ogrn: Union[str, None] = None
     xml: bool = False
+
+    @validator('ogrn')
+    def check_a_or_b(cls, v, values):
+        if 'inn' not in values and not values['ogrn']:
+            raise ValueError('either a or b is required')
+        return values['ogrn']
--- endpoints/counterparty/routes.py
+++ endpoints/counterparty/routes.py
@@ -1,14 +1,48 @@
-#from .models import ConturAPIGet
-from config import CONTUR_FOCUS_API_URL, CONTUR_FOCUS_API_VER, CONTUR_FOCUS_API_KEY
+from app_logger import logger
+from .models import ConturAPIGet
+from config import CONTUR_FOCUS_API_URL, CONTUR_FOCUS_API_VER, CONTUR_FOCUS_API_KEY, API_KEY
+from fastapi import APIRouter, status, HTTPException
 import requests
+import traceback
+
+
+router = APIRouter(
+    prefix='/counterpartycheck',
+    tags=['CounterpartyCheck'],
+    responses={404: {'detail': 'Not found'}},
+)
+
+
+@router.get('/')
+async def counterparty_check(input_data: ConturAPIGet):
+    apikey_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='API key not valid'
+    )
+    badparams_exception = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail='Request parameters not valid'
+    )
+    if input_data.api_key != API_KEY:
+        logger.error(f'Error accessing API with: {str(input_data)}')
+        raise apikey_exception
+    try:
+        pass
+    except Exception as err:
+        lf = '\n'
+        logger.error(f'{traceback.format_exc().replace(lf, "")} : {str(err)}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{traceback.format_exc()} : {str(err)}')
 
 
 if __name__ == '__main__':
-    API_FUNCTION = '/stat'
+    API_FUNCTION = '/req'
     final_url = CONTUR_FOCUS_API_URL + CONTUR_FOCUS_API_VER + API_FUNCTION
     params = {
-                'key': CONTUR_FOCUS_API_KEY
+                'key': CONTUR_FOCUS_API_KEY,
+                'inn': '3123169789'
              }
     response_contur_api = requests.get(final_url, params=params)
     if response_contur_api.status_code == 200:
         print(response_contur_api.json())
+    else:
+        logger.error(f'Error getting ConturAPI request: {response_contur_api.status_code} - {response_contur_api.headers}')
